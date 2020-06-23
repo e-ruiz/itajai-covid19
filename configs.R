51a015958f527675b5e437946b2b1ea73c1fa0c0
Eric S. Lucinger Ruiz <e-ruiz@users.noreply.github.com>
2020-06-22 21:57:19 -0300

    Create configs.R

---
+++ configs.R
@@ -0,0 +1,12 @@
+#
+# Credentials to deploy on 
+# https://www.shinyapps.io
+#
+shinyapps_token = '<super_token>'
+shinyapps_secret = '<super_secret>'
+shinyapps_name = 'app_name'
+
+#
+# Token de acesso a API Itajaí
+#
+api_itajai_token = 'super_token'
